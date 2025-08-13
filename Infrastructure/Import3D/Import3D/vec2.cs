c71286596e06fce755912aba2dba1cf86f484e1b
bitzhuwei <bitzhuwei@qq.com>
2025-08-13 14:03:44 +0800

    public struct vec2 {

---
+++ Infrastructure/Import3D/Import3D/vec2.cs
@@ -0,0 +1,19 @@
+﻿namespace Import3D {
+    public struct vec2 {
+        public float x;
+        public float y;
+
+        public vec2(float value) {
+            this.x = value;
+            this.y = value;
+        }
+        public vec2(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString() {
+            return $"{x}, {y}";
+        }
+    }
+}
